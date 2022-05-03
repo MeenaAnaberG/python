8bea380371da5c83975116cb56e67227e1055a41
MeenaAnaberG <meena.anaber.g@gmail.com>
2022-05-03 17:00:38 +0530

    hello

---
+++ loop.py
@@ -0,0 +1,50 @@
+n=10
+sum=0
+i=1
+
+while i<=n:
+    sum=sum+i
+    i=i+1
+print("the sum is",sum)
+
+
+
+
+for val in "string":
+    if val=="i":
+        continue
+    print(val)
+
+print("the end")
+
+
+
+
+counter=0
+while counter < 3:
+    print("inside loop")
+    counter = counter+1
+else:
+    print("inside else")
+
+
+
+
+for val in "string":
+    if val=="i":
+        continue
+    print(val)
+
+print("the end")
+
+
+
+
+def greet(name):
+    print("hello "+ name +" good morning")
+greet('meena')
+
+
+
+
+print(greet("may"))
